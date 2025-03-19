d3df0a699e1dea3d9dd396290a9d8b8682eca194
Petro <petrotymchuck@gmail.com>
2025-03-19 08:15:39 +0200

    config header

--- js/loader.js
+++ js/loader.js
@@ -3,4 +3,24 @@
     xhr.open("GET", path, false);
     xhr.send();
     document.write(xhr.response);
+
+    if (path.includes("header")) {
+        document.addEventListener("DOMContentLoaded", () => {
+            toggleUserMenu();
+        });
+    }
+};
+
+function toggleUserMenu() {
+    const token = localStorage.getItem("token");
+    const anonimMenu = document.getElementById("anonimMenu");
+    const loginMenu = document.getElementById("loginMenu");
+
+    if (token) {
+        anonimMenu.classList.add("hidden");
+        loginMenu.classList.remove("hidden");
+    } else {
+        anonimMenu.classList.remove("hidden");
+        loginMenu.classList.add("hidden");
+    }
 }
